2a784e5c0e3f61f5097b8b2e6dceb2b9dab4ea30
dilsha01 <mihirangangd.21@uom.lk>
2025-02-14 10:04:45 +0530

    Update App

--- auth-app-website/src/App.js
+++ auth-app-website/src/App.js
@@ -3,12 +3,14 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Dashboard from "./pages/Dashboard";
 import RedirectHandler from "./handler";
+import SignIn from "./pages/SignIn";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<SignIn />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/assert" element={<RedirectHandler />} />
       </Routes>
