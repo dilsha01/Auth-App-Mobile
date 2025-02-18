6fdca11fdacc71bbb0203c0228a34d8fe5d16ced
dilsha01 <mihirangangd.21@uom.lk>
2025-02-18 14:49:46 +0530

    Update LogoutButton

--- auth-app-mobile/app/components/LogoutButton.tsx
+++ auth-app-mobile/app/components/LogoutButton.tsx
@@ -3,9 +3,11 @@ import { Button, IconButton } from "react-native-paper";
 import { Linking } from "react-native";
 
 const LogoutButton = () => {
+  	const BASE_URL = "http://172.20.10.6:4000";
+
   const handleLogout = () => {
     // Redirect to SAML backend logout route
-    Linking.openURL("http://localhost:4000/logout")
+    Linking.openURL(`${BASE_URL}/logout`)
       .catch((err) => console.error("Failed to log out: ", err));
   };
 
